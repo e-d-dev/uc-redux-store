b7456d3d0250cf2c7f9c2e666a0e247ae75bcd3f
E.D. <edeibert@protonmail.com>
2025-09-18 09:52:13 +0200

    Chapter "Writing our own Redux Store" - Actions to Action Creators

--- src/app.ts
+++ src/app.ts
@@ -18,12 +18,9 @@ button.addEventListener(
   () => {
     if (!input.value.trim()) return;
 
-    const payload = { label: input.value, complete: false };
+    const todo = { label: input.value, complete: false };
 
-    store.dispatch({
-      type: "ADD_TODO",
-      payload, // shorthand syntax, creating a property with the same name as a variable (exactly the same as: "payload: payload")
-    });
+    store.dispatch(new fromStore.AddTodo(todo));
 
     input.value = "";
   },
@@ -39,7 +36,8 @@ destroy.addEventListener("click", unsubscribe, false);
 todoList.addEventListener("click", function (event) {
   const target = event.target as HTMLButtonElement;
   if (target.nodeName.toLowerCase() === "button") {
-    console.log(target);
+    const todo = JSON.parse(target.getAttribute("data-todo") as any);
+    store.dispatch(new fromStore.RemoveTodo(todo));
   }
 });
 
